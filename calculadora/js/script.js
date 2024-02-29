331d3f51cb6f971d9e265a400ad30b6656cf6251
lucas martins <124801819+lucasmpseven@users.noreply.github.com>
2024-02-29 11:07:50 -0300

    Add files via upload

---
+++ calculadora/js/script.js
@@ -0,0 +1,8 @@
+var resultado = document.getElementById('resultado')
+
+function peganome(){
+    var nome = document.getElementById('nome').value
+    //console.log('nome:', nome)
+    resultado.innerHTML = nome
+
+}
