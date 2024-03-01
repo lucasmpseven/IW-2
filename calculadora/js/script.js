4749aab469df65972dbad2330e0e3fa8c1d8b981
lucas martins <124801819+lucasmpseven@users.noreply.github.com>
2024-02-29 21:06:32 -0300

    Add files via upload

--- calculadora/js/script.js
+++ calculadora/js/script.js
@@ -1,8 +1,31 @@
+var n1 = document.getElementById('n1')
+var n1 = document.getElementById('n2')
 var resultado = document.getElementById('resultado')
 
-function peganome(){
-    var nome = document.getElementById('nome').value
-    //console.log('nome:', nome)
-    resultado.innerHTML = nome
+function soma(){
+    resultado.innerHTML = parseFloat(n1.value) + parseFloat(n2.value)
+}
 
+function sub(){
+    resultado.innerHTML = parseFloat(n1.value) - parseFloat(n2.value)
+}
+
+function mult(){
+    resultado.innerHTML = parseFloat(n1.value) * parseFloat(n2.value)
+}
+
+function div(){
+
+    if (n1.value === 0) {
+        resultado.innerHTML = "Erro, número 0 não divide!";
+    } else{
+        resultado.innerHTML = parseFloat(n1.value) / parseFloat(n2.value)
+    }
+
+}
+
+function limpar(){
+    n1.value = "";
+    n2.value = "";
+    resultado.innerHTML = "";
 }
